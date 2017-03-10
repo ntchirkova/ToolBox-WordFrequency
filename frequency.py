52f3f072c52a30f609d8476b010c2001349dbb67
Nina Tchirkova <ntchirkova@gmail.com>
2017-03-10 14:00:14 -0500

    Gets top 100 words of Pride and Prejudice

--- frequency.py
+++ frequency.py
@@ -2,15 +2,23 @@
 Project Gutenberg """
 
 import string
+import pickle
 
+input_file = open('austen.pickle', 'rb')
+book = pickle.load(input_file)
 
-def get_word_list(file_name):
+def get_word_list(book):
     """ Reads the specified project Gutenberg book.  Header comments,
     punctuation, and whitespace are stripped away.  The function
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    #strips the header
+    start = book.index('***START OF THE PROJECT GUTENBERG EBOOK PRIDE AND PREJUDICE***') + len('***START OF THE PROJECT GUTENBERG EBOOK PRIDE AND PREJUDICE***')
+    book = book[start:len(book)]
+    book = book.lower()
+    word_list = book.split()
+    return word_list
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +31,13 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    d = dict()
+    for w in word_list:
+        d[w] = d.get(w, 0) + 1
+    ordered_by_frequency = sorted(d, key=d.get, reverse=True)
+    return ordered_by_frequency[0:n]
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    word_list = get_word_list(book)
+    print(get_top_n_words(word_list, 100))
